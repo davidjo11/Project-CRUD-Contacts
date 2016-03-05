782c4ffec769faed9927602b40979a517a0a6282
root <dav.josias@gmail.com>
2016-03-05 22:19:06 +0100

    Register ongoing

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 28/01/16
+ * Time: 15:49
+ */
+
+namespace AppBundle\Controller;
+
+
+class UserController
+{
+
+}
---
+++ src/AppBundle/User/Register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 28/01/16
+ * Time: 15:07
+ */
+
+namespace AppBundle\User;
+
+
+class Register
+{
+
+}
